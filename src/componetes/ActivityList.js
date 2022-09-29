ec9c860ed9fde562632e5b8f3724299fc89e7777
AnamikaSa <amanika0061@gmail.com>
2022-09-30 01:52:21 +0600

    question part added

--- src/componetes/ActivityList.js
+++ src/componetes/ActivityList.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
-const ActivityList = ({activity, handleAdd}) => {
+const ActivityList = ({ activity, handleAdd }) => {
     const { title, img, age, time, details } = activity;
     return (
-        <div className='w-64'>
-            <img className='rounded-lg h-52' src={img} alt="" />
-            <p className='font-extrabold mt-5'>{title} <br /></p>
-            <p className='mt-5'>{details}</p> <br />
-            <p className='font-bold'>For Age: {age}</p>
-            <p className='font-bold'>Time required: {time} <span>s</span></p>
-            <button onClick={()=>handleAdd(activity)} className='mt-6 rounded bg-fuchsia-500 p-2 w-full hover:bg-purple-400'>
-                <p>Add to list</p>
-            </button>
+        <div>
+            <div className='w-64'>
+                <img className='rounded-lg h-52' src={img} alt="" />
+                <p className='font-extrabold mt-5'>{title} <br /></p>
+                <p className='mt-5'>{details}</p> <br />
+                <p className='font-bold'>For Age: {age}</p>
+                <p className='font-bold'>Time required: {time} <span>s</span></p>
+                <button onClick={() => handleAdd(activity)} className='mt-6 rounded bg-fuchsia-500 p-2 w-full hover:bg-purple-400'>
+                    <p>Add to list</p>
+                </button>
+            </div>
+
         </div>
+
     );
 };
 
